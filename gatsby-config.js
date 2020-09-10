bff8c14f5aa95429a3a6d4903d3393cf06470516
Patrick Youssef <patricksyoussef@gmail.com>
2020-09-10 07:45:54 -0700

    Began styling and created filler blog posts.

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,21 +4,21 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `pages`,
-        path: `${__dirname}/src/pages`
+        path: `${__dirname}/src/pages`,
       },
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `posts`,
-        path: `${__dirname}/src/posts`
+        path: `${__dirname}/src/posts`,
       },
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`
+        path: `${__dirname}/src/images`,
       },
     },
     `gatsby-plugin-sharp`,
@@ -26,7 +26,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-mdx`,
       options: {
-        extensions: [".md",".mdx"],
+        extensions: [".md", ".mdx"],
         gatsbyRemarkPlugins: [
           {
             resolve: `gatsby-remark-images`,
@@ -40,8 +40,12 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-fonts`,
       options: {
-        fonts: [`lato`, `karla\:400,400i,700,700i`],
-        display: "swap"
+        fonts: [
+          `ibm plex sans`,
+          `open sans`,
+          `inconsolata`
+        ],
+        display: "swap",
       },
     },
   ],
---
+++ src/themes/theme.js
@@ -0,0 +1,31 @@
+export default {
+  fonts: {
+    main: "IBM Plex Sans, sans-serif",
+    code: "Inconsolata, monospace"
+  },
+  colors: {
+    main1: "hsl(205, 70%, 60%)",
+    dark1: "hsl(225, 2%, 10%)",
+    light1: "hsl(0,0%,97%)",
+  },
+  breakpoints: {
+    mobile: "only screen and (max-width: 50rem)",
+    tablet: "only screen and (max-width: 65rem)",
+  },
+  spacings: {
+    xxsmall: "0.25rem",
+    xsmall: "0.5rem",
+    small: "1rem",
+    medium: "2rem",
+    large: "3rem",
+    xlarge: "4rem",
+    xxlarge: "6rem",
+  },
+  animations: {
+    button: "box-shadow 0.3s ease",
+    link: "color 0.2s ease",
+  },
+  shadows: {
+    shadow1: "0px 5px 20px rgba(30, 30, 31, 0.05)",
+  },
+}
