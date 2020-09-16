170ebceb7ea02737e7188b51c6a7ec40d06dd3bd
Patrick Youssef <patricksyoussef@gmail.com>
2020-09-16 00:57:17 -0700

    Refactoring blog organization for slug generation.

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,19 @@
 const path = require(`path`)
 const _ = require("lodash")
+const { createFilePath } = require(`gatsby-source-filesystem`)
+
+exports.onCreateNode = ({ node, actions, getNode }) => {
+  const { createNodeField } = actions
+  if (node.internal.type === `Mdx`) {
+    const value = createFilePath({ node, getNode })
+    console.log(value)
+    createNodeField({
+      name: `slug`,
+      node,
+      value,
+    })
+  }
+}
 
 exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
