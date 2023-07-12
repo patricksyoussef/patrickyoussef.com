d32b91ec4866240c149cb57ddd73434154afdef5
Patrick Youssef <patricksyoussef@gmail.com>
2023-07-11 18:58:29 -0700

    Added back the loader animation

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,3 +4,13 @@ import 'sanitize.css/typography.css';
 export { default as wrapPageElement } from "./src/gatsby_apis/wrapPageElement";
 export { default as wrapRootElement } from "./src/gatsby_apis/wrapRootElement";
 
+export const onInitialClientRender = () => {
+  const loadTimer = 50
+  const animTimer = loadTimer + 100
+  setTimeout(function () {
+    document.getElementById("___loader").style.opacity = "0"
+  }, loadTimer)
+  setTimeout(function () {
+    document.getElementById("___loader").style.display = "none"
+  }, animTimer)
+}
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -8,14 +8,14 @@ const GlobalStyles = createGlobalStyle(({ theme }) => `
   @font-face {
     font-family: "DM Sans";
     font-style: normal;
-    font-display: block;
+    font-display: swap;
     font-weight: 200 300 400 500 700;
     src: url("/fonts/DMSansVar.ttf");
   }
   @font-face {
     font-family: "Fira Code";
     font-style: normal;
-    font-display: block;
+    font-display: swap;
     font-weight: 400;
     src: url("/fonts/FiraCode-Regular.ttf");
   }
